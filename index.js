50770d3ff11dc26b9687833548fb2aa5398368f9
simeontambu <simeontambu@gmail.com>
2023-07-27 12:08:54 +0100

    random message algorithm in index.js

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+// Define arrays containing data
 
-// hello
+const subjects = ["You", "I", "We"];
+const verbs = ["are amazing", "can do it", "will succeed"];
+const objects = ["today!", "in everything you do.", "no matter what happens."];
+
+// Function to generate a random index
+const getRandomIndex = (array) => {
+  return Math.floor(Math.random() * array.length);
+};
+// Generate a random message
+
+const generateMessage = () => {
+  const subject = subjects[getRandomIndex(subjects)];
+  const verb = verbs[getRandomIndex(verbs)];
+  const object = objects[getRandomIndex(objects)];
+
+  return `${subject} ${verb} ${object}`;
+};
+
+console.log(generateMessage());
